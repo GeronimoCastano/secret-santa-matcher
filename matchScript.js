9d0dc8995c8b00f143c010b909a8c6280a04c37b
GeronimoCastano <111615495+GeronimoCastano@users.noreply.github.com>
2024-03-04 16:41:16 +0100

    Add files via upload

---
+++ matchScript.js
@@ -0,0 +1,108 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add-button');
+    const nameInput = document.getElementById('name-input');
+    const emailInput = document.getElementById("email-input");
+    const participantsList = document.getElementById('participants-list');
+    const generateMatchesButton = document.getElementById("generate-match-button");
+
+function isValidEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+
+
+const addParticipant = () => {
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    if (name != "") {
+    if (isValidEmail(email)) {
+        const participantDiv = document.createElement('div');
+        participantDiv.className = 'person';
+        participantDiv.dataset.email = email;
+        
+        const p = document.createElement('p');
+        p.className = 'name';
+        p.textContent = name;
+        
+        const editButton = document.createElement('button');
+        editButton.className = 'edit-icon-button';
+        editButton.innerHTML = '<img class="edit-icon" src="https://www.svgrepo.com/show/522527/edit-3.svg">';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete-icon-button';
+        deleteButton.innerHTML = '<img class="delete-icon" src="https://www.svgrepo.com/show/513658/cross.svg">';
+        deleteButton.onclick = function() {
+        participantsList.removeChild(participantDiv);
+        };
+
+        participantDiv.appendChild(p);
+        participantDiv.appendChild(editButton);
+        participantDiv.appendChild(deleteButton);
+
+        participantsList.appendChild(participantDiv);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+    else if (!isValidEmail(email)) {
+        window.alert("Please enter a Valid Email");
+        emailInput.value = '';
+    }
+    
+    }
+};
+
+    addButton.addEventListener('click', addParticipant);
+    nameInput.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+        addParticipant();
+        }
+    });
+    emailInput.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+        addParticipant();
+        }
+    });
+
+    function extractParticipants() {
+        const participantElements = document.querySelectorAll('.person');
+        const participantsLookup = {};
+    
+        participantElements.forEach(el => {
+            const name = el.querySelector('.name').textContent.trim();
+            const email = el.dataset.email; // Assuming you stored the email as a data attribute
+            participantsLookup[name] = email;
+        });
+    
+        return participantsLookup;
+    }
+
+    function storeParticipantsLookup() {
+        const participantsLookup = extractParticipants();
+        localStorage.setItem('participantsLookup', JSON.stringify(participantsLookup));
+        console.log("Participants lookup stored");
+        console.log(participantsLookup);
+        return participantsLookup;
+    }
+    
+
+    generateMatchesButton.addEventListener('click', () => {
+        const participantNames = storeParticipantsLookup();
+        if (Object.keys(participantNames).length > 0) {
+            window.location.href = 'pairup.html';
+        } else {
+            alert('Please add participants before generating matches.');
+        }
+    });
+
+});
+
+
+
+  
+
+
+
+
+
---
+++ pairupScript.js
@@ -0,0 +1,88 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    emailjs.init({
+        publicKey: 'H7mOFYYfBdQ4wI7Ty',
+    });
+    
+    const storedData = localStorage.getItem('participantsLookup');
+    const participantNames = JSON.parse(storedData);
+    console.log(participantNames);
+
+
+
+    function updateAfterPairing() {
+        const title = document.querySelector('.loading-container h1');
+        title.textContent = 'Done! The participants have each received an email with their correspondent matches.';
+      
+        const loadingBar = document.querySelector('.loading-bar');
+        loadingBar.style.display = 'none';
+    }
+    
+    
+    function generateMatches() {
+    let names = Array.from(Object.keys(participantNames)); // Make sure it's an array
+    let shuffled;
+    do {
+        // Shuffle array
+        shuffled = names
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
+    
+        // Check if any name matches the same index after shuffle
+    } while (names.some((name, index) => name === shuffled[index]));
+    
+    // Pair names
+
+    let result = {}
+
+    for (let i = 0; i < shuffled.length; i++) {
+        result[names[i]] = shuffled[i];
+    }
+
+    return result;
+    }
+    
+    const progress = document.querySelector('.loading-progress');
+
+
+    
+    setTimeout(() => {
+        progress.style.width = '100%';
+    }, 2000);
+    setTimeout (() => {
+        updateAfterPairing();
+    }, 5000);
+
+    const pairs = generateMatches();
+    console.log(pairs);
+
+    function sendEmails(pairs, participantNames) {
+        Object.entries(pairs).forEach(([santa, recipient]) => {
+            
+            const recipientEmail = participantNames[recipient];
+    
+
+            emailjs.send('service_p4wsdc2', 'template_nsvgk4k', {
+                sender_name: "_",
+                sender_email: "gerocastano8@gmail.com",
+                recipient_name: recipient,
+                recipient_email: recipientEmail,
+                message: `Hello ${recipient},\n\nYou have been matched with ${santa} for the Secret Santa gift exchange.\n\nHappy gifting!\n\nBest regards,\nThe Secret Santa Team`
+            })
+            .then((response) => {
+                console.log('Email sent successfully:', response);
+            }, (error) => {
+                console.error('Error sending email:', error);
+            });
+        });
+    }
+    
+
+    sendEmails(pairs, participantNames);
+});
+
+
